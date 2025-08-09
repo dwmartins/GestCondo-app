3fedf2cdeecdc89523aa75952464901efe9082b1
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2025-08-08 23:21:36 -0300

    feat: desenvolvido a tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gestcondo/utils/app_colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,92 +10,160 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool remember_me = false;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: Card(
-          color: Colors.white,
-          elevation: 2,
-          margin: EdgeInsets.all(10),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("GestCondo", style: TextStyle(fontSize: 32)),
-                const SizedBox(height: 40),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: "E-mail",
-                    labelStyle: TextStyle(color: Colors.blue),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: "Senha",
-                    labelStyle: TextStyle(color: Colors.blue),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: remember_me,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          remember_me = value ?? false;
-                        });
-                      }, 
-                    ),
-                    const Text("Lembrar de mim"),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Aqui a lógica do login
-                     
-                    },
-                    child: const Text("Entrar"),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/cover.png',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+
+          Center(
+            child: Card(
+              margin: EdgeInsets.all(15),
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: SizedBox(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Conecte-se',
+                        style: TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textColor
+                        ),
+                      ),
+                      const Text(
+                        'Por favor insira seus dados',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: AppColors.textColor
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      SizedBox(
+                        height: 50,
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'E-mail',
+                            prefixIcon: Icon(
+                              Icons.email_outlined,
+                              size: 20,
+                              color: AppColors.textColor,
+                            ),
+                            floatingLabelStyle: TextStyle(
+                              color: AppColors.primary,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: AppColors.primary),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      SizedBox(
+                        height: 50,
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            prefixIcon: Icon(
+                              Icons.lock_outline,
+                              size: 20,
+                              color: AppColors.textColor,
+                            ),
+                            floatingLabelStyle: TextStyle(
+                              color: AppColors.primary,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: AppColors.primary),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                print('Esqueci minha senha clicado');
+                              },
+                              child: const Text(
+                                'Esqueci minha senha',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+                      
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primary,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                            ),
+                          ),
+                          onPressed: () {
+                            print("Login efetuado");
+                          },
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff409EFF);
+  static const Color primary50 = Color(0xfff0f9ff);
+  static const Color primary100 = Color(0xffe0f2fe);
+  static const Color primary200 = Color(0xffbae6fd);
+  static const Color primary300 = Color(0xff7dd3fc);
+  static const Color primary400 = Color(0xff38bdf8);
+  static const Color primary500 = Color(0xff409EFF);
+  static const Color primary600 = Color(0xff0284c7);
+  static const Color primary700 = Color(0xff0369a1);
+  static const Color primary800 = Color(0xff075985);
+  static const Color primary900 = Color(0xff0c4a6e);
+  static const Color primary950 = Color(0xff082f49);
+
+  static const Color textColor = Color(0xFF334155);
+}
